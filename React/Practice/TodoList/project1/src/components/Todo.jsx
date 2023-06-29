1a7d260c5710001a0a5ee724ee2662af2fc2c5d8
hani <ploo18800@gmail.com>
2023-06-29 22:23:03 +0900

    React 할 일 목록 구현

---
+++ React/Practice/TodoList/project1/src/components/Todo.jsx
@@ -0,0 +1,25 @@
+const Todo = ({text, todo, condition, btnText, removeTodo, changeState}) => {
+    return (
+        <>
+            <h1>{text}</h1>
+            <div className='main_top'>
+                {todo.filter((t) => {return t.isDone === condition}).map((item) => {
+                    return (
+                        <div className='main_middle'>
+                            <div>
+                                <h1>{item.title}</h1>
+                                <span>{item.content}</span>
+                            </div>
+                            <div className='main_middle_btn'>
+                                <button className='remove_btn' onClick={() => {removeTodo(item.id)}}>삭제하기</button>
+                                <button className='done_btn' onClick={() => changeState(item)}>{btnText}</button>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        </>
+    )
+};
+
+export default Todo
