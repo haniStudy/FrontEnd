5e98d9208f10f8d822faf5c5f19a2b214a959b86
hani <ploo18800@gmail.com>
2023-07-04 10:35:09 +0900

    리액트 할 일 목록 커스텀 훅 적용

--- React/Practice/TodoList/project2/src/components/Header.jsx
+++ React/Practice/TodoList/project2/src/components/Header.jsx
@@ -1,16 +1,12 @@
-import React, {useCallback, useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import { addTodo } from '../redux/modules/todoList';
+import { useInput } from '../hooks/useInput';
 
 const Header = ({todoList, dispatch}) => {
-    // 사용자한테 입력을 받을 때 한번만 렌더링 시키기 위해 useCallback hook 사용
-    const [title, setTitle] = useState(''); // 할 일 제목
-    const handleChangeTitle = useCallback((e) => {setTitle(e.target.value)}, []);
+    const [title, setTitle, handleChangeTitle] = useInput(''); // 할 일 제목
+    const [content, setContent, handleChangeContent] = useInput(''); // 할 일 내용
 
-    const [content, setContent] = useState(''); // 할 일 내용
-    const handleChangeContent = useCallback((e) => {setContent(e.target.value)}, []);
-
-    // 새로운 할 일 목록이 추가 되면 입력되었던 값 초기화
-    useEffect(() => {
+    useEffect(() => { // 새로운 할 일 목록이 추가 되면 입력되었던 값 초기화
         setTitle("");
         setContent("");
     }, [todoList]);
@@ -20,7 +16,6 @@ const Header = ({todoList, dispatch}) => {
             <section className='header-title'>
                 <span>TodoList</span>
             </section>
-
             <section className='header-section'>
                 <div className='header-section-div'>
                     <label> 제목 </label> <input type='text' value={title}   onChange={handleChangeTitle} />
