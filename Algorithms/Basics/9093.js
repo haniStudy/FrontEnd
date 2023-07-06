469e0e9dc5ac2088210164bc381a948c0c71becb
hani <ploo18800@gmail.com>
2023-07-06 22:33:24 +0900

    알고리즘 문제 풀이: 백준_9093

---
+++ Algorithms/Basics/9093.js
@@ -0,0 +1,10 @@
+// https://www.acmicpc.net/problem/9093
+
+const input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+
+let len = parseInt(input[0]);
+for (let i = 1; i <= len; i++) {
+  let words = input[i].split(" ");
+  let answer = words.map((word) => word.split("").reverse().join("")).join(" ");
+  console.log(answer);
+}
