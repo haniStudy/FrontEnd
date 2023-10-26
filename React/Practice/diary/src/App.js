495ce321249deed0e0124f153c7534b7be3f0cbf
hani <ploo18800@gmail.com>
2023-10-26 14:59:45 +0900

    리액트 공부

---
+++ React/Practice/diary/src/App.js
@@ -0,0 +1,8 @@
+import './App.css';
+import DiaryEdit from "./DiaryEdit";
+
+function App() {
+    return <DiaryEdit />;
+}
+
+export default App;
---
+++ React/Practice/diary/src/DiaryEdit.js
@@ -0,0 +1,64 @@
+import { useRef, useState } from "react";
+
+const DiaryEdit = () => {
+    // html DOM 요소에 접근할 수 있게 해줌
+    const authorRef = useRef();
+    const contentRef = useRef();
+
+    const [state, setState] = useState({
+        author: "", // 작성자
+        content: "", // 내용
+        emotion: 1, // 감정 점수
+    });
+
+    const handleChangeState = (e) => {
+        const val = e.target;
+        setState({
+            ...state,
+            [val.name]: val.value,
+        });
+    };
+
+    const handleSubmit = () => {
+        if (state.author.length < 1) {
+            // 현재 가리키는 값을 가져와서 사용!
+            authorRef.current.focus();
+            return ;
+        }
+
+        if (state.content.length < 5) {
+            contentRef.current.focus();
+            return;
+        }
+
+        alert("저장 성공");
+    };
+
+    return (
+        <div className="DiaryEdit">
+            <h2>오늘의 일기</h2>
+            {/* onChange는 값이 바뀌었을 때 수행하는 이벤트 */}
+            <div>
+                <input ref={authorRef} name="author" value={state.author} onChange={handleChangeState}/>
+            </div>
+            <div>
+                <textarea ref={contentRef} name="content" value={state.content} onChange={handleChangeState}/>
+            </div>
+            <div>
+                오늘의 감정점수 : 
+                <select name="emotion" value={state.emotion} onChange={handleChangeState}>
+                    <option value={1}>1</option>
+                    <option value={2}>2</option>
+                    <option value={3}>3</option>
+                    <option value={4}>4</option>
+                    <option value={5}>5</option>
+                </select>
+            </div>
+            <div>
+                <button onClick={handleSubmit}>일기 저장하기</button>
+            </div>
+        </div>
+    )
+}
+
+export default DiaryEdit;
