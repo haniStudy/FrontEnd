a7fa8d7fcd3262d50aeec591851ac11b3590653f
hani <134919218+haniStudy@users.noreply.github.com>
2023-11-13 10:01:49 +0900

    리액트 공부

--- React/Practice/diary/src/App.js
+++ React/Practice/diary/src/App.js
@@ -1,12 +1,37 @@
-import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState, useReducer } from "react";
 
 import './App.css';
 import DiaryEdit from "./DiaryEdit";
 import DiaryList from "./DiaryList";
 import OptimizeTest from "./OptimizeTest";
 
+const reducer = (state, action) => {
+    switch(action.type) {
+        case "INIT": {
+            return action.data; // 새로운 state return
+        }
+        case "CREATE": {
+            const created_date = new Date().getTime();
+            const newItem = {
+                ...action.data,
+                created_date
+            }
+            return [newItem, ...state];
+        }
+        case "REMOVE": {
+            return state.filter((it)=>{it.id !== action.targetId});
+        }
+        case "EDIT": {
+            return state.map((it)=>{it.id === action.targetId ? {...it, content: action.newContent} : it});
+        }
+        default:
+            return state;
+    }
+};
+
 function App() {
-    const [datas, setDatas] = useState([]);
+    // const [datas, setDatas] = useState([]);
+    const [datas, dispatch] = useReducer(reducer, []);
 
     // 비동기 함수로 만들기
     const getData = async() => {
@@ -21,7 +46,8 @@ function App() {
                 id: dataId.current++,
             }
         });
-        setDatas(initData);
+        // setDatas(initData);
+        dispatch({type: "INIT", data: initData});
     };
 
     // 컴포넌트가 마운트 되는 시점에 실행
@@ -30,28 +56,32 @@ function App() {
     }, []);
 
     const dataId = useRef(0);
-    const onCreate = useCallback((author, content, emotion) => { // 일기 추가
-        const created_date = new Date().getTime(); // 현재 시간
-        const newData = {
-            author,
-            content,
-            emotion,
-            created_date, 
-            id: dataId.current,
-        };
+    const onCreate = useCallback((author, content, emotion) => { // 일기 추가        
+        // const created_date = new Date().getTime(); // 현재 시간
+        // const newData = {
+        //     author,
+        //     content,
+        //     emotion,
+        //     created_date, 
+        //     id: dataId.current,
+        // };
+        // setDatas((data)=>[newData, ...data]); // 기존 리스트에 새로운 값 추가
+
+        dispatch({type: "CREATE", data: {author, content, emotion, id: dataId.current}});
         dataId.current++;
-        setDatas((data)=>[newData, ...data]); // 기존 리스트에 새로운 값 추가
     }, []);
 
     const onRemove = useCallback(
         (targetId) => { // 일기 삭제
             console.log(`${targetId}가 삭제되었습니다.`);
-            setDatas(data => data.filter((d)=> d.id !== targetId));
+            // setDatas(data => data.filter((d)=> d.id !== targetId));
+            dispatch({type: "REMOVE", targetId});
         }, []);
 
     const onEdit = useCallback(
         (targetId, newContent) => { // 일기 수정
-            setDatas(data => data.map((d)=>d.id === targetId ? {...d, content: newContent}: d));
+            // setDatas(data => data.map((d)=>d.id === targetId ? {...d, content: newContent}: d));
+            dispatch({type: "EDIT", targetId});
         }, []);
 
     // 연산 최적화
