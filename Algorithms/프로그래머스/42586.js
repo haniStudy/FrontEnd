821e8d2c80a61a525b8575695e7a4bbdffb7eb24
hani <ploo188000@gmail.com>
2023-12-11 20:17:19 +0900

    JS 코딩 테스트 준비

--- Algorithms/프로그래머스/42586.js
+++ Algorithms/프로그래머스/42586.js
@@ -1,5 +1,6 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=javascript
 
+// 1차
 function solution(progresses, speeds) {
     let temps = [];  // 임시로 값을 담을 변수
     // for문을 이용해서 배열의 요소에 접근
@@ -27,4 +28,24 @@ function solution(progresses, speeds) {
         }
     }
     return results;
+}
+
+// 2차
+function solution(progresses, speeds) {
+    let answer = [];
+    let len = progresses.length; // 길이 값 저장
+    while(speeds.length > 0){ // 원소가 모두 제거되면 반복문 종료
+        for(s in speeds){ // for ~ in 구문 사용하여 인덱스 가져와 일치하는 인덱스에 위치한 요소 값 더해줌
+            progresses[s] += speeds[s];
+        }
+        
+        let count = 0; 
+        while(progresses[0] >= 100) { // 첫 번째 값이 100보다 클 때 까지만 반복
+            progresses.shift(); // 첫 번째 값이 100 보다 크면 제거
+            speeds.shift(); // 위와 동일
+            count++; // 카운터 증가
+        }
+        if(count > 0) answer.push(count);
+    }
+    return answer;
 }
