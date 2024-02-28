01d1a0137a26282666e848ed8197e6e8bbe1aaaa
hani <ploo188000@gmail.com>
2024-02-28 17:05:10 +0900

    [JS] 코딩 테스트 준비

--- Algorithms/프로그래머스/133499.js
+++ Algorithms/프로그래머스/133499.js
@@ -17,3 +17,13 @@ function solution(babbling) {
     
     return answer;
 }
+
+// 모범 담안
+function solution(babbling) {
+    const regexp1 = /(aya|ye|woo|ma)\1+/;
+    const regexp2 = /^(aya|ye|woo|ma)+$/;
+  
+    return babbling.reduce((ans, word) => (
+      !regexp1.test(word) && regexp2.test(word) ? ++ans : ans
+    ), 0);
+}
